a30528612d5cb1bfbe3ff1c0c930f5ac40690697
Ramon Rocha <ramonrocha.cavalcanti@gmail.com>
2025-04-03 18:26:58 -0300

    test: Completed validation tests for ExpenseValidator

--- test/Validators.Tests/Expenses/Register/ExpenseValidatorTests.cs
+++ test/Validators.Tests/Expenses/Register/ExpenseValidatorTests.cs
@@ -1,5 +1,7 @@
 ﻿using CommonTestUtilities.Requests;
 using FinanceControlAPI.Application.UseCases.Expenses;
+using FinanceControlAPI.Communication.Enums;
+using FinanceControlAPI.Exception;
 using FluentAssertions;
 
 namespace Validators.Tests.Expenses.Register;
@@ -15,5 +17,62 @@ public class ExpenseValidatorTests
     var result = validator.Validate(request);
 
     result.IsValid.Should().BeTrue();
+  }
+
+  [Theory]
+  [InlineData("")]
+  [InlineData("  ")]
+  [InlineData(null)]
+  public void ErrorTitleEmpty(string title)
+  {
+    var validator = new ExpenseValidator();
+    var request = RegisterExpenseRequestBuilder.Build();
+    request.Title = title;
+
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+    result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.TITLE_REQUIRED));
+  }
+
+  [Fact]
+  public void ErrorDateFuture()
+  {
+    var validator = new ExpenseValidator();
+    var request = RegisterExpenseRequestBuilder.Build();
+    request.Date = DateTime.UtcNow.AddDays(1);
+
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+    result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.EXPENSES_CANNOT_FOR_THE_FUTURE));
+  }
+
+  [Fact]
+  public void ErrorPaymentTypeInvalid()
+  {
+    var validator = new ExpenseValidator();
+    var request = RegisterExpenseRequestBuilder.Build();
+    request.PaymentType = (PaymentType)70;
+
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+    result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.PAYMENT_TYPE_INVALID));
+  }
+
+  [Theory]
+  [InlineData(0)]
+  [InlineData(-1)]
+  public void ErrorAmountInvalid(decimal amount)
+  {
+    var validator = new ExpenseValidator();
+    var request = RegisterExpenseRequestBuilder.Build();
+    request.Amount = amount;
+
+    var result = validator.Validate(request);
+
+    result.IsValid.Should().BeFalse();
+    result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO));
   }
 }
