f391a8efbf4bbcc42950508ea6b302c223790df8
Ramon Rocha <ramonrocha.cavalcanti@gmail.com>
2025-04-03 16:45:45 -0300

    feat: added resource localization middleware and config in Program

--- src/FinanceControlAPI.API/Program.cs
+++ src/FinanceControlAPI.API/Program.cs
@@ -1,3 +1,4 @@
+using FinanceControlAPI.API.Middleware;
 using FinanceControlAPI.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,6 +18,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<CultureMiddleware>();
 
 app.UseHttpsRedirection();
 
