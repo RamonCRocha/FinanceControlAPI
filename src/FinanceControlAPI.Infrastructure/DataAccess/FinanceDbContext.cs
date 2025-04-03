b9e457bbbe542712b1524acfc022db85bdd1624d
Ramon Rocha <ramonrocha.cavalcanti@gmail.com>
2025-04-03 15:38:08 -0300

    feat: added DbContext with DbSet configuration

---
+++ src/FinanceControlAPI.Infrastructure/DataAccess/FinanceDbContext.cs
@@ -0,0 +1,10 @@
+﻿using FinanceControlAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceControlAPI.Infrastructure.DataAccess;
+public class FinanceDbContext : DbContext
+{
+  public FinanceDbContext(DbContextOptions options) : base(options) { }
+
+  public DbSet<Expense> Expenses { get; set; }
+}
