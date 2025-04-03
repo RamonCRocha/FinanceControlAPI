23d99303948ff819ed0c7c971e9efc2cc1224429
Ramon Rocha <ramonrocha.cavalcanti@gmail.com>
2025-04-03 17:46:16 -0300

    feat: added CommonTestUtilities with Bogus integration

---
+++ test/CommonTestUtilities/Requests/RegisterExpenseRequestBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using FinanceControlAPI.Communication.Enums;
+using FinanceControlAPI.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+public class RegisterExpenseRequestBuilder
+{
+  public RegisterExpenseRequest Build()
+  {
+    return new Faker<RegisterExpenseRequest>()
+      .RuleFor(r => r.Title, faker => faker.Commerce.ProductName())
+      .RuleFor(r => r.Description, faker => faker.Commerce.ProductDescription())
+      .RuleFor(r => r.Date, faker => faker.Date.Past())
+      .RuleFor(r => r.PaymentType, faker => faker.PickRandom<PaymentType>())
+      .RuleFor(r => r.Amount, faker => faker.Random.Decimal(min: 1, max: 1000));
+  }
+}
