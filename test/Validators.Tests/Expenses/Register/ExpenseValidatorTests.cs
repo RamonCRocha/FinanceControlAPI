cc312201e1f5eb8ff02c8dd3c699367417a745c5
Ramon Rocha <ramonrocha.cavalcanti@gmail.com>
2025-04-03 17:32:28 -0300

    feat: added unit test for ExpenseValidator

---
+++ test/Validators.Tests/Expenses/Register/ExpenseValidatorTests.cs
@@ -0,0 +1,24 @@
+﻿using FinanceControlAPI.Application.UseCases.Expenses;
+using FinanceControlAPI.Communication.Requests;
+
+namespace Validators.Tests.Expenses.Register;
+public class ExpenseValidatorTests
+{
+  [Fact]
+  public void Success()
+  {
+    var validator = new ExpenseValidator();
+
+    var request = new RegisterExpenseRequest
+    {
+      Amount = 100,
+      Date = DateTime.Now.AddDays(-1),
+      Title = "Test",
+      PaymentType = FinanceControlAPI.Communication.Enums.PaymentType.DebitCard
+    };
+
+    var result = validator.Validate(request);
+
+    Assert.True(result.IsValid);
+  }
+}
