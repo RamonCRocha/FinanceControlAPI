45de645bf98560e726f977fd98c71f32be2c4fa7
Ramon Rocha <ramonrocha.cavalcanti@gmail.com>
2025-04-03 16:13:18 -0300

    feat: implemented repositories and UnitOfWork in infrastructure

---
+++ src/FinanceControlAPI.Infrastructure/DataAccess/Repositories/ExpensesRepository.cs
@@ -0,0 +1,50 @@
+﻿using FinanceControlAPI.Domain.Entities;
+using FinanceControlAPI.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceControlAPI.Infrastructure.DataAccess.Repositories;
+internal class ExpensesRepository : IExpensesReadRepository, IExpensesWriteRepository, IExpensesUpdateRepository
+{
+  private readonly FinanceDbContext _dbContext;
+
+  public ExpensesRepository(FinanceDbContext dbContext)
+  {
+    _dbContext = dbContext;
+  }
+
+  public async Task Add(Expense expense)
+  {
+    await _dbContext.Expenses.AddAsync(expense);
+  }
+
+  public async Task<bool> Delete(long id)
+  {
+    var result = await _dbContext.Expenses.FirstOrDefaultAsync(x => x.Id == id);
+    if (result is null)
+      return false;
+
+    _dbContext.Expenses.Remove(result);
+
+    return true;
+  }
+
+  public async Task<List<Expense>> GetAll()
+  {
+    return await _dbContext.Expenses.AsNoTracking().ToListAsync();
+  }
+
+  async Task<Expense?> IExpensesReadRepository.GetById(long id)
+  {
+    return await _dbContext.Expenses.AsNoTracking().FirstOrDefaultAsync(e => e.Id == id);
+  }
+
+  async Task<Expense?> IExpensesUpdateRepository.GetById(long id)
+  {
+    return await _dbContext.Expenses.FirstOrDefaultAsync(e => e.Id == id);
+  }
+
+  public void Update(Expense expense)
+  {
+    _dbContext.Expenses.Update(expense);
+  }
+}
---
+++ src/FinanceControlAPI.Infrastructure/DataAccess/UnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using FinanceControlAPI.Domain.Repositories;
+
+namespace FinanceControlAPI.Infrastructure.DataAccess;
+internal class UnitOfWork : IUnitOfWork
+{
+  private readonly FinanceDbContext _dbContext;
+
+  public UnitOfWork(FinanceDbContext dbContext)
+  {
+    _dbContext = dbContext;
+  }
+
+  public async Task Commit() => await _dbContext.SaveChangesAsync();
+}
