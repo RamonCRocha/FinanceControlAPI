762fe62b5d7e817c9d73f5562ce8d7122ac1bbbd
Ramon Rocha <ramonrocha.cavalcanti@gmail.com>
2025-04-17 18:16:46 -0300

    feat: added UsersController with POST method for user registration

---
+++ src/FinanceControlAPI.API/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using FinanceControlAPI.Application.UseCases.Users.Register;
+using FinanceControlAPI.Communication.Requests;
+using FinanceControlAPI.Communication.Responses;
+using FinanceControlAPI.Communication.Responses.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceControlAPI.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+  [HttpPost]
+  [ProducesResponseType(typeof(RegisterUserResponse), StatusCodes.Status201Created)]
+  [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
+  public async Task<IActionResult> Register([FromServices] IRegisterUserUseCase useCase, [FromBody] RegisterUserRequest request)
+  {
+    var response = await useCase.Execute(request);
+
+    return Created(string.Empty, response);
+  }
+}
