bc7f88748bcc713d4185a6418c5effbc52cdde25
Ramon Rocha <ramonrocha.cavalcanti@gmail.com>
2025-05-09 19:55:10 -0300

    feat: add JWT Bearer token support to Swagger UI

--- src/FinanceControlAPI.API/Extensions/AuthenticationExtension.cs
+++ src/FinanceControlAPI.API/Extensions/AuthenticationExtension.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 namespace FinanceControlAPI.API.Extensions;
@@ -9,6 +10,7 @@ public static class AuthenticationExtension
   public static void AddProjectConfigurations(this IServiceCollection services, IConfiguration configuration)
   {
     ConfigureJwtAuthentication(services, configuration);
+    ConfigureSwagger(services);
   }
 
   private static void ConfigureJwtAuthentication(IServiceCollection services, IConfiguration configuration)
@@ -28,6 +30,40 @@ public static class AuthenticationExtension
         ClockSkew = new TimeSpan(0),
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey!))
       };
+    });
+  }
+
+  private static void ConfigureSwagger(IServiceCollection services)
+  {
+    services.AddSwaggerGen(config =>
+    {
+      config.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+      {
+        Name = "Authorization",
+        Description = @"JWT Authorization header using the Bearer scheme.
+                        Enter 'Bearer' [space] and then your token in the text input below.
+                        Example: 'Bearer 1234abcdef'",
+        In = ParameterLocation.Header,
+        Scheme = "Bearer",
+        Type = SecuritySchemeType.ApiKey
+      });
+      config.AddSecurityRequirement(new OpenApiSecurityRequirement
+      {
+        {
+          new OpenApiSecurityScheme
+          {
+            Reference = new OpenApiReference
+            {
+              Type = ReferenceType.SecurityScheme,
+              Id = "Bearer"
+            },
+            Scheme = "oauth2",
+            Name = "Bearer",
+            In = ParameterLocation.Header
+          },
+           new List<string>()
+        }
+      });
     });
   }
 }
