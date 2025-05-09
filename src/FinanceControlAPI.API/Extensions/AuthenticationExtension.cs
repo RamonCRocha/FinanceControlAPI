9c19bb779389bb4594bb2e7c2adc2c942ad4c149
Ramon Rocha <ramonrocha.cavalcanti@gmail.com>
2025-05-09 19:52:41 -0300

    feat: add JWT authentication configuration using JwtBearer

--- src/FinanceControlAPI.API/Extensions/AuthenticationExtension.cs
+++ src/FinanceControlAPI.API/Extensions/AuthenticationExtension.cs
@@ -1,9 +1,33 @@
-﻿namespace FinanceControlAPI.API.Extensions;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace FinanceControlAPI.API.Extensions;
 
 public static class AuthenticationExtension
 {
   public static void AddProjectConfigurations(this IServiceCollection services, IConfiguration configuration)
   {
+    ConfigureJwtAuthentication(services, configuration);
+  }
 
+  private static void ConfigureJwtAuthentication(IServiceCollection services, IConfiguration configuration)
+  {
+    var signingKey = configuration.GetValue<string>("Settings:Jwt:SigningKey");
+
+    services.AddAuthentication(config =>
+    {
+      config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+      config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(config =>
+    {
+      config.TokenValidationParameters = new TokenValidationParameters
+      {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ClockSkew = new TimeSpan(0),
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey!))
+      };
+    });
   }
 }
