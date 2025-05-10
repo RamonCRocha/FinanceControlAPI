97bc78b3febbb50c3fa0be7d030e18f2492de2d0
Ramon Rocha <ramonrocha.cavalcanti@gmail.com>
2025-05-10 07:25:11 -0300

    test: add RegisterUserRequestBuilder with Faker for user request generation

---
+++ test/CommonTestUtilities/Requests/RegisterUserRequestBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Bogus;
+using FinanceControlAPI.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+public class RegisterUserRequestBuilder
+{
+  public static RegisterUserRequest Build()
+  {
+    return new Faker<RegisterUserRequest>()
+      .RuleFor(user => user.Name, faker => faker.Person.FirstName)
+      .RuleFor(user => user.Email, (faker, user) => faker.Internet.Email(user.Name))
+      .RuleFor(user => user.Password, faker => faker.Internet.Password(prefix: "!Aa1"));
+  }
+}
