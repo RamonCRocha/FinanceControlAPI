08ee9ef3205b449c6913766d9b9e67d04f92f769
Ramon Rocha <ramonrocha.cavalcanti@gmail.com>
2025-05-10 07:34:08 -0300

    test: add unit tests for invalid password scenarios using PasswordValidator

---
+++ test/Validators.Tests/Users/PasswordValidatorTests.cs
@@ -0,0 +1,33 @@
+﻿using FinanceControlAPI.Application.UseCases.Users;
+using FinanceControlAPI.Communication.Requests;
+using FluentAssertions;
+using FluentValidation;
+
+namespace Validators.Tests.Users;
+public class PasswordValidatorTests
+{
+  [Theory]
+  [InlineData("  ")]
+  [InlineData("")]
+  [InlineData(null)]
+  [InlineData("a")]
+  [InlineData("aa")]
+  [InlineData("aaa")]
+  [InlineData("aaaa")]
+  [InlineData("aaaaa")]
+  [InlineData("aaaaaa")]
+  [InlineData("aaaaaaa")]
+  [InlineData("aaaaaaaa")]
+  [InlineData("AAAAAAAA")]
+  [InlineData("Aaaaaaaa")]
+  [InlineData("Aaaaaaa1")]
+  public void Error_Password_Invalid(string password)
+  {
+    var validator = new PasswordValidator<RegisterUserRequest>();
+
+    var result = validator
+      .IsValid(new ValidationContext<RegisterUserRequest>(new RegisterUserRequest()), password);
+
+    result.Should().BeFalse();
+  }
+}
